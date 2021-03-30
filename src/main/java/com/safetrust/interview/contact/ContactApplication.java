2cbd2164d0cd477101a3e6ec3f1645f6b5b887dd
ngocbao <nngocbao@tma.com.vn>
2021-03-30 18:58:33 +0700

    Config dependdency

--- src/main/java/com/safetrust/interview/contact/ContactApplication.java
+++ src/main/java/com/safetrust/interview/contact/ContactApplication.java
@@ -2,8 +2,15 @@ package com.safetrust.interview.contact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * 
+ * ContactApplication is used to initialize application
+ *
+ */
 @SpringBootApplication
+@ComponentScan( { "com.safetrust.interview.contact" } )
 public class ContactApplication {
 
 	public static void main(String[] args) {
